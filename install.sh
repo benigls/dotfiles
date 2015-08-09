a4aa1c6c5b7c3a269fd04091996544506b9a67cd
Ben <iglesia.ben@gmail.com>
2015-08-09 13:41:28 +0800

    Handle changes on vim files.

--- install.sh
+++ install.sh
@@ -1,36 +1,45 @@
+#!/bin/bash
+
 echo "Installing dotfiles..."
 
+echo "Initializing submodule(s)"
+git submodule update --init --recursive
+
 DOTFOLDER="$HOME/.dotfiles"
-VIMRC=".vimrc"
 ZSHRC=".zshrc"
 TMUXCONF=".tmux.conf"
 GITCONF=".gitconfig"
-
+DIR=""
 
 if [ ! -d "$DOTFOLDER" ]; then
     mkdir "$DOTFOLDER"
 fi
 
-echo "Installing vim configurations."
+echo "Creating symlinks"
+linkables=$( ls -1 -d **/*.symlink )
 
-cp $VIMRC $DOTFOLDER
-ln -s $DOTFOLDER/$VIMRC $HOME/$VIMRC
+for file in $linkables ; do
+    name="$( basename $file ".symlink" )"
+    target="$HOME/.$name"
+    echo "Creating symlink for $file"
 
-echo "Installing zsh configurations."
+    if [ -d "$file" ] ; then
+       DIR="-r"
+    fi
+
+    cp $DIR $file $DOTFOLDER/$name
+    ln -s $DOTFOLDER/$name $target
+
+    DIR=""
+done
 
 cp $ZSHRC $DOTFOLDER
 ln -s $DOTFOLDER/$ZSHRC $HOME/$ZSHRC
 
-echo "Installing tmux configurations."
-
 cp $TMUXCONF $DOTFOLDER
 ln -s $DOTFOLDER/$TMUXCONF $HOME/$TMUXCONF
 
-echo "Installing git configurations."
-
 cp $GITCONF $DOTFOLDER
 ln -s $DOTFOLDER/$GITCONF $HOME/$GITCONF
-
-
 
 echo "All dotfiles installed"
