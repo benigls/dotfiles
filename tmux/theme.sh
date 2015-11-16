d7d681fb99bd953a1eca3f5e96b1d5a471d5bc9d
Ben <iglesia.ben@gmail.com>
2015-11-16 10:19:15 +0000

    Add theme to tmux.

---
+++ tmux/theme.sh
@@ -0,0 +1,47 @@
+# TODO: Fix colors and layout
+tm_icon="☀"
+tm_color_active=colour02
+tm_color_inactive=colour06
+tm_color_feature=colour04
+tm_active_border_color=colour08
+
+set -g status-left-length 32
+set -g status-right-length 150
+set -g status-interval 5
+
+# default statusbar colors
+set-option -g status-fg $tm_color_active
+set-option -g status-bg default
+set-option -g status-attr default
+
+# default window title colors
+set-window-option -g window-status-fg $tm_color_inactive
+set-window-option -g window-status-bg default
+set -g window-status-format "#I #W"
+
+# active window title colors
+set-window-option -g window-status-current-fg $tm_color_active
+set-window-option -g window-status-current-bg default
+set-window-option -g  window-status-current-format "#[bold]#I #W"
+
+# pane border
+set-option -g pane-border-fg $tm_color_inactive
+set-option -g pane-active-border-fg $tm_active_border_color
+
+# message text
+set-option -g message-bg default
+set-option -g message-fg $tm_color_active
+
+# pane number display
+set-option -g display-panes-active-colour $tm_color_active
+set-option -g display-panes-colour $tm_color_inactive
+
+# clock
+set-window-option -g clock-mode-colour $tm_color_active
+
+tm_date="#[fg=$tm_color_inactive] %R %d %b"
+tm_host="#[fg=$tm_color_feature,bold]#h"
+tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
+
+set -g status-left $tm_session_name' '
+set -g status-right $tm_date' '$tm_host
