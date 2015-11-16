0506eff524413aeec0098e831d7539bf8311b567
Ben <iglesia.ben@gmail.com>
2015-11-16 03:13:32 +0000

    Refactor install and uninstall.

--- install.sh
+++ install.sh
@@ -5,11 +5,8 @@ echo "Installing dotfiles..."
 echo "Initializing submodule(s)"
 git submodule update --init --recursive
 
-DOTFOLDER="$HOME/.dotfiles"
-GITCONF=".gitconfig"
-
-if [ ! -d "$DOTFOLDER" ]; then
-    cp -r ../dotfiles $DOTFOLDER
+if [ ! -d $DOTFILES ]; then
+    cp -r ../dotfiles $DOTFILES
 fi
 
 echo "Creating symlinks"
@@ -19,13 +16,10 @@ for file in $linkables ; do
     target="$HOME/.$( basename $file ".symlink" )"
     echo "Creating symlink for $file"
 
-    ln -s $DOTFOLDER/$file $target
+    ln -s $DOTFILES/$file $target
 done
-
-cp $GITCONF $DOTFOLDER
-ln -s $DOTFOLDER/$GITCONF $HOME/$GITCONF
 
 echo "Instaling .config files."
 cp -r config/* $HOME/.config/
 
-echo "All dotfiles installed"
+echo "All dotfiles are installed."
--- uninstall.sh
+++ uninstall.sh
@@ -1,21 +1,27 @@
-echo "Uninstalling dotfiles..."
-# TODO: Refactor this. Reverse the process of installation.
-DOTFOLDER="$HOME/.dotfiles"
-VIMRC=".vimrc"
-ZSHRC=".zshrc"
-TMUXCONF=".tmux.conf"
-GITCONF=".gitconfig"
-CONFIG=".config/base16-shell" #TODO: Make it flexible when their are many config folder.
+#!/bin/bash
 
-if [ -d "$DOTFOLDER" ]; then
-    rmf "$DOTFOLDER"
+echo "Uninstalling dotfiles..."
+
+if [ -d $DOTFILES ]; then
+    rm -rf $DOTFILES
 fi
 
-rm $HOME/$VIMRC
-rm $HOME/$ZSHRC
-rm $HOME/$TMUXCONF
-rm $HOME/$GITCONF
-rmf $HOME/.vim
-rmf $HOME/$CONFIG
+echo "Removing symlinks."
+linkables=$( ls -1 -d **/*.symlink )
 
-echo "All dotfiles uninstalled"
+for file in $linkables ; do
+    target="$HOME/.$( basename $file ".symlink" )"
+    rm -rf $target
+done
+
+echo "Removing .config files."
+configs=$( ls -1 -d config/* )
+
+for file in $configs ; do
+    target="$HOME/.config/$( basename $file )"
+    rm -rf $target
+done
+
+unset file linkables configs target
+
+echo "All dotfiles are uninstalled."
