ef29bcea55d54400b47b9079a8a4a5e34ba1af81
Ben <iglesia.ben@gmail.com>
2018-03-29 22:38:11 +0800

    Update apt-get aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,12 +15,12 @@ alias llv="ls -1 $VAGRANT_FOLDER"
 alias rmf="rm -rf"
 
 # apt-get
-alias ins="sudo apt-get install "
-alias rem="sudo apt-get remove --purge "
+alias ins="sudo apt-get -y install"
+alias rem="sudo apt-get remove --purge"
 
-alias upd="sudo apt-get update"
-alias upg="sudo apt-get upgrade"
-alias up="upd && upg"
+alias upd="sudo apt-get -y update"
+alias upg="sudo apt-get -y upgrade"
+alias up="sudo apt-get -y update && sudo apt-get -y upgrade"
 
 alias v="vim"
 
